440919e81ea295b9c1091adde03f1fc8a4996cb5
Dominick Triola <dominick@ignovadesign.com>
2016-12-23 22:58:14 -0800

    create basic react component

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,1 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Visualizer from './visualizer.jsx';
 
+document.addEventListener("DOMContentLoaded", () => {
+	const container = document.getElementById("container");
+	ReactDOM.render(<Visualizer />, container);
+});
---
+++ frontend/visualizer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Visualizer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        visualizer content
+      </div>
+    );
+  }
+}
+
+export default Visualizer;
