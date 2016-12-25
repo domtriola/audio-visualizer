ad6781906f2d8eb4a831c0c84c8755c01b277913
Dominick Triola <dominick@ignovadesign.com>
2016-12-24 16:52:16 -0800

    configure visualizer to render with store colors

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -3,44 +3,47 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import store from './store';
-import VisualizerContainer from './components/visualizer_container.js';
+import Visualizer from './components/visualizer.jsx';
 import ControlsContainer from './components/controls_container.js';
 
 // TODO debugging
 window.store = store;
 
 if (window.location.pathname === "/visualizers/") {
-  const App = () => (
-  	<Provider store={store}>
-      <div>
-        <VisualizerContainer />
-        <ControlsContainer />
-      </div>
-  	</Provider>
-  );
-
   document.addEventListener("DOMContentLoaded", () => {
-    // // Setup Canvas
-    // const canvas = document.createElement('canvas');
-    // const ctx = canvas.getContext('2d');
-    // canvas.width = 600;
-    // canvas.height = 300;
-    // canvas.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
-    // document.body.appendChild(canvas);
-    //
-    // // Setup Audio
-    // const audio = document.createElement('audio');
-    // audio.style.width = 300 + 'px';
-    // audio.src = '/assets/Tours_-_01_-_Enthusiast.mp3';
-    // audio.controls = 'true';
-    // document.body.appendChild(audio);
-    //
-    // // Setup Audio Analyser
-    // const audioContext = new AudioContext();
-    // const analyser = audioContext.createAnalyser();
-    // const source = audioContext.createMediaElementSource(audio);
-    // source.connect(analyser);
-    // analyser.connect(audioContext.destination);
+    // Setup Canvas
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 600;
+    canvas.height = 300;
+    canvas.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+    document.body.appendChild(canvas);
+
+    // Setup Audio
+    const audio = document.createElement('audio');
+    audio.style.width = 300 + 'px';
+    audio.src = '/assets/Tours_-_01_-_Enthusiast.mp3';
+    audio.controls = 'true';
+    document.body.appendChild(audio);
+
+    // Setup Audio Analyser
+    const audioContext = new AudioContext();
+    const analyser = audioContext.createAnalyser();
+    const source = audioContext.createMediaElementSource(audio);
+    source.connect(analyser);
+    analyser.connect(audioContext.destination);
+
+    const App = () => (
+      <Provider store={store}>
+        <div>
+          <Visualizer store={store}
+                      canvas={canvas}
+                      ctx={ctx}
+                      analyser={analyser}/>
+          <ControlsContainer />
+        </div>
+      </Provider>
+    );
 
   	ReactDOM.render(
   		<App />,
--- frontend/components/visualizer.jsx
+++ frontend/components/visualizer.jsx
@@ -1,10 +1,58 @@
 import React from 'react';
 
-const Visualizer = ({ctx}) => (
-    <div>
-      <canvas width="600" height="300"></canvas>
-      <audio src=""></audio>
-    </div>
-  );
+class Visualizer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      freqByteData: new Uint8Array(this.props.analyser.frequencyBinCount)
+    };
+
+    this.draw();
+  }
+
+  draw() {
+    this.colors = this.props.store.getState();
+    this.props.analyser.getByteFrequencyData(this.state.freqByteData);
+    this.props.ctx.clearRect(0, 0, 600, 300);
+
+    this.equalizerBar();
+
+    requestAnimationFrame(this.draw.bind(this));
+  }
+
+  equalizerBar() {
+    const canvasHeight = this.props.canvas.height;
+    const freqByteData = this.state.freqByteData;
+    let barWidth = this.props.canvas.width /
+                   this.state.freqByteData.length * 8;
+
+    for (var i = 1; i < freqByteData.length; i += barWidth) {
+      let red = this.colors.red,
+          green = this.colors.green,
+          blue = this.colors.blue;
+
+      this.props.ctx.lineWidth = 1;
+      this.props.ctx.strokeStyle = '#000000';
+      this.props.ctx.fillStyle = 'rgb(' + red + ',' + green + ',' + blue + ')';
+
+      this.props.ctx.fillRect(i,
+        canvasHeight - freqByteData[Math.floor(i)],
+        barWidth, canvasHeight);
+      this.props.ctx.strokeRect(i,
+        canvasHeight - freqByteData[Math.floor(i)],
+        barWidth, canvasHeight);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <p>testing...</p>
+      </div>
+    );
+  }
+}
+
 
 export default Visualizer;
